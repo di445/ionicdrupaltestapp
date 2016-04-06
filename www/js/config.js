9fa5c5992bd65b0382cf3724cf08c05d99725a48
di445 <gregtyka@gmail.com>
2016-04-06 18:24:52 -0400

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -3,9 +3,9 @@ var app_config = angular.module('drupalionic.configuration', []);
 
 /**
 * REPLACE HERE YOUR DRUPAL INSTANCE URL AND API ENDPOINT DEFINED ON YOUR SERVICES
-* drupal_instance could be 'http://dev-example.pantheon.io/'
-* api_endpoint could be: api/v1/
+* drupal_instance could be 'http://drupal-1001.rhcloud.com/'
+* api_endpoint could be: simplyfccapp
 **/
-app_config.constant('drupal_instance','http://dev-dia-vida.pantheon.io/');
-app_config.constant('api_endpoint','api/v1/');
+app_config.constant('drupal_instance','http://drupal-1001.rhcloud.com/');
+app_config.constant('api_endpoint','simplyfccapp');
 
